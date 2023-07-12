d9396c66e2c087763a8eb87f7bc3d76b8c722da9
ZaharGasyuk <zahargasiyk@gmail.com>
2023-07-12 11:03:49 +0300

    first commit

---
+++ lesson1/main.py
@@ -0,0 +1,26 @@
+# print("hello Zahar")
+# print("10+10")
+# print(10+10)
+# print(10-10)
+# print(10*10)
+# print(10**10)
+# print(10/10)
+# print(10//10)
+# a = 10
+# b = 10.5
+# c = "10.5"
+# print(type(a),a)
+# a = float(a)
+# print(type(a),a)
+# a = str(a)
+# print(type(a),a)
+# a = int(a)
+# e = True
+# d = False
+# print(10<20)
+# print(10==20)
+# print(True<False)
+a = b = c = d = 10
+c = 1
+print(c)
+print(a)
