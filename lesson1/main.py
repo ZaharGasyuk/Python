74702a017528c0af28c936ad1cecafb3e8c60dc6
ZaharGasyuk <zahargasiyk@gmail.com>
2023-07-19 11:07:09 +0300

    uppdate

--- lesson1/main.py
+++ lesson1/main.py
@@ -20,7 +20,100 @@
 # print(10<20)
 # print(10==20)
 # print(True<False)
-a = b = c = d = 10
-c = 1
-print(c)
-print(a)
+# a = b = c = d = 10
+# c = 1
+# print(c)
+# print(a)
+
+
+
+
+
+
+
+
+
+
+# Gasyuk = "Zahar"
+# print(len(Gasyuk))
+
+
+
+# number = 12.5
+# converted = int(number)
+# print(converted)
+
+
+# number = 27
+# converted = float(number)
+# print(converted)
+
+
+# numer = int(input("15: "))
+# number = float(numer)
+# print(number)
+
+# num = int(input("Bведіть ціле число: "))
+# float_num = float(num)
+# print("Pезультат конвертації:", float_num)
+
+
+# number = float(input("введіть число: ")) 
+# square = number ** 2 
+# print("квадрат числа", number, "дорівнює", square)
+
+
+# Число = 33 
+# float_num = 5.55 
+# СТрінг = "Hello Zahar" 
+# Falser = False 
+# results  = "int: {}\nfloat: {}\nstr: {}\nбулеве значення: {}".format(Число, float_num, СТрінг, Falser)
+# print(results)
+
+
+# Число = 33 
+# float_num = 5.55 
+# СТрінг = "Hello Zahar" 
+# Falser = False 
+# results  = "int: float: str: булеве значення: ".format(Число, float_num, СТрінг, Falser)
+# print(results)
+
+
+# valera = input("Введіть будь ласочка: ")
+# romko = valera.upper()
+# print("Рядочок у верхньому регістрі: ", romko)
+
+# valera = input("Введіть будь ласочка: ")
+# romko = valera.upper()
+# print("Рядочок у верхньому регістрі: ")
+
+# mercedes = input("Введіть будь ласочка рядочок у верхньому регістрі: ")
+# bmw = mercedes.lower()
+# print("Маєш рядочок у нижньому регістрі: ", bmw)
+
+
+# olexandr = input("Пишіть: ")
+# zahar = olexandr[::-1]
+# print("Маєш", zahar)
+
+
+# номер1 = float(input("введіть число номер 1: "))
+# номер2 = float(input("введіть число номер 2: "))
+# total = номер1 + номер2
+# print("Маєш", total)
+
+# номер1 = float(input("введіть число номер 1: "))
+# номер2 = float(input("введіть число номер 2: "))
+# Різниця = номер1 - номер2
+# print("Маєш", Різниця)
+
+# python  = input("Пиши: ")
+# boom = python[::-1]
+# if python.lower() == boom.lower():
+#     print("є")
+# else: print("не є.")
+
+name1 = "Olexandr"
+name2 = "Zahar"
+result = "Доброго дня, {0}! як справи?, Доброго, {1} , ти знаєш що сьогодні середа?". format(name1, name2 ) 
+print(result)
---
+++ lesson2/main.py
@@ -0,0 +1,60 @@
+# a = 10
+# aaaa = float(a)
+
+# name = "zahar"
+# aaaa = name.upper()
+
+# print(aaaa)
+# b = aaaa.casefold()
+# print(b)
+
+# for i in reversed(range(10)):
+#     print(i)
+
+# a =int(input("Write number: "))
+# print(type(a))
+# print(a+1)
+# mylist = [1,2,3,4,5]
+# mylist.append(10)
+# print(mylist)
+
+# mylist = [1,2,3,4,5]
+# mylist.clear()
+# print(mylist)
+
+# mylist = [1,2,3,4,5]
+# a = mylist.copy()
+# print(a)
+
+# mylist = [1,1,2,3,4,5]
+# print(mylist.count(4))
+# mylist.append(6)
+# print(mylist)
+
+# mylist = [1,2,3,4,5]
+# mylist.pop(0)
+# print(mylist)
+
+# mylist = [1,2,3,4,5]
+# mylist.remove(5)
+# print(mylist)
+
+# mylist = [1,2,3,4,5]
+# mylist.insert(len(mylist) ,100)
+# print(mylist)
+
+# mylist = [1,2,3,4,5]
+# mylist.extend([1,1,1])
+# print(mylist)
+
+
+mylist = [1,2,3,4,5]
+print(mylist.index(1))
+mylist = []
+for i in range(1,6):
+    # print(i,end='')
+    mylist.append(i)
+print(mylist)
+name = 'zahar'
+mylist.insert(-1,name.upper())
+print(mylist)
